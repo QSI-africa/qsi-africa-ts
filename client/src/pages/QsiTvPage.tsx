c576c1c0bf18e24e98cf3fd88ad1f38a13aecd9b
Elvisk2710 <tadiwakadeya@gmail.com>
2026-04-24 21:28:47 +0200

    fix: import useState in QsiTvPage

--- client/src/pages/QsiTvPage.tsx
+++ client/src/pages/QsiTvPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { 
   Layout, Row, Col, Typography, Button, 
   Empty, Badge, Card, Space as AntSpace 
