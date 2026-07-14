f659896e4be364228d0109ea757fda5fcd775524
Your Name <kadeyaelvis@gmail.com>
2026-07-14 13:49:29 +0200

    chore: use Google public STUN servers for WebRTC

--- server/index.js
+++ server/index.js
@@ -112,22 +112,11 @@ app.get("/api/health", (req, res) => {
 app.get("/api/ice-config", (req, res) => {
   res.json({
     iceServers: [
-      { urls: ['stun:stun1.l.google.com:19302', 'stun:stun2.l.google.com:19302'] },
-      {
-        urls: 'turn:openrelay.metered.ca:80',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-      },
-      {
-        urls: 'turn:openrelay.metered.ca:443',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-      },
-      {
-        urls: 'turn:openrelay.metered.ca:443?transport=tcp',
-        username: 'openrelayproject',
-        credential: 'openrelayproject'
-      }
+      { urls: 'stun:stun.l.google.com:19302' },
+      { urls: 'stun:stun1.l.google.com:19302' },
+      { urls: 'stun:stun2.l.google.com:19302' },
+      { urls: 'stun:stun3.l.google.com:19302' },
+      { urls: 'stun:stun4.l.google.com:19302' }
     ]
   });
 });
